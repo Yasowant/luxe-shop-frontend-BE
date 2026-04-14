81acc7856874d1cc4ccd8f21954b0e32f10f2013
Yasowant N <yasowant1998@gmail.com>
2026-04-14 18:26:12 +0530

    updaed code

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -92,7 +92,7 @@ app.use(
     target: process.env.AUTH_SERVICE_URL,
     changeOrigin: true,
     pathRewrite: {
-      "^/api/auth": "/api/auth", // ✅ keep same path
+      "^/api/auth": "", // ✅ REMOVE PREFIX
     },
   }),
 );
